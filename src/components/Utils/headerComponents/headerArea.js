16c2c55af83e220b466a6c185e93265a4ce96a4b
kain <s.e.jugr@gmail.com>
2020-02-20 21:57:08 -0600

    header in progress === 10% :skull:

---
+++ src/components/Utils/headerComponents/headerArea.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const HeaderArea = styled.div`
+left: 0;
+    right: 0;
+    width: 100%;
+    top: 0;
+    z-index: 9;
+    position: absolute;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+`
+
+export default HeaderArea;
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,15 @@
-import { Link } from "gatsby"
 import React from "react"
-
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `black`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+import HeaderArea from '../components/Utils/headerComponents/headerArea'
+import Container from '../components/Utils/headerComponents/container'
+const Header = () => (
+  <header>
+    <HeaderArea>
+      <div style={{padding:'12px 0', backGround:'#fff'}}>
+        <Container>
+          <div>hola bb</div>
+        </Container>
+      </div>
+    </HeaderArea>
   </header>
 )
 
