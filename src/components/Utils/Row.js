453164c0724d56656c5b7293379539b8f687686e
kain <s.e.jugr@gmail.com>
2020-02-22 13:10:57 -0600

    feature/layout.css updated & Row component :skull:

---
+++ src/components/Utils/Row.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+
+const Row = styled.div`
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+`
+
+export default Row;
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,20 @@
 import React from "react"
 import HeaderArea from '../components/Utils/headerComponents/headerArea'
 import Container from '../components/Utils/headerComponents/container'
+import Row from '../components/Utils/Row'
 const Header = () => (
   <header>
     <HeaderArea>
       <div style={{padding:'12px 0', backGround:'#fff'}}>
         <Container>
-          <div>hola bb</div>
+          <Row>
+          <div className='col-xl-5 col-md-5'>
+            <div className='hader_left'>
+              <p>Welcome to Conbusi consulting service</p>
+            </div>
+          </div>
+
+          </Row>
         </Container>
       </div>
     </HeaderArea>
